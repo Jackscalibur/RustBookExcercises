7081ffd9747a15aa8179c582f22c2971c17d32a3
Jackson Cowdrey <jackson.taylor.cowdrey@gmail.com>
2024-04-21 19:09:12 -0500

    finish guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -9,33 +9,31 @@ fn main() {
 
     // This will let the user guess as many times as needed
     loop {
-        // !!! Put main logic inside of loop !!!
-    }
-    println!("Please input your guess.");
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        // This could induce an Error with an empty String reference being passed in normal
-        // circumstances
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        io::stdin()
+            // This could induce an Error with an empty String reference being passed in normal
+            // circumstances
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Please enter a valid number.");
-            return; // Exit the program if parsing fails
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // Allow the user to continue guessing
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-    };
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
     }
-
     println!("The secret number was {secret_number}");
 }
